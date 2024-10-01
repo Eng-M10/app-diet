71f28209b55d9d9e2af5162f6d2d3f7869ba6938
Muvimbene Daniel Maposse <muvi10.com@gmail.com>
2024-10-01 11:42:10 +0200

    fix: fixture register feat

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::put('/update/user/{id}',[AuthController::class,'update']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 

437718f47edaa31d3d7684defc2eacfc60b6e55f
Muvimbene Daniel Maposse <muvi10.com@gmail.com>
2024-10-05 12:03:44 +0200

    fix: fixture sugestoes feat

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,5 @@ Route::put('/update/user/{id}',[AuthController::class,'update']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->put('/update-password', [AuthController::class, 'updatePassword']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
 Route::middleware('auth:sanctum')->get('/sugestoes/{id}', [SugestaoController::class, 'sugerirRefeicao']);
 Route::middleware('auth:sanctum')->get('/informacoes/{id}', [AuthController::class, 'getInfo']);
